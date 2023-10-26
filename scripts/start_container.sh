494bc81814cef4d2e32fc5e29b012ccb6a79e889
adnanitdev <115599232+adnanitdev@users.noreply.github.com>
2023-10-26 13:32:18 -0400

    add appspec

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull docker pull adnan3594/aws-code-build:latest
+
+# Run the Docker image as a container
+docker run -d -p 8080:8080 docker pull adnan3594/aws-code-build:latest
