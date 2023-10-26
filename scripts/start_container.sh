f286473f5d524969646b97b04b18640d3ea5b0fe
adnanitdev <115599232+adnanitdev@users.noreply.github.com>
2023-10-26 14:09:26 -0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull adnan3594/aws-code-build:latest
+docker pull adnan3594/aws-code-build:latest
 
 # Run the Docker image as a container
 docker run -d -p 8080:8080 docker pull adnan3594/aws-code-build:latest
